e7e58820b8f567e7b0b276ea3afa9ea7dbe0b83e
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2024-12-17 10:55:26 -0800

    introduction to java

---
+++ introduction/introduction.java
@@ -0,0 +1,6 @@
+// Q: Why you must learn Java
+// java run 60 billions devices 
+// java usage for mobile apps , web apps , backend and enterprise level application
+// High paying jobs 
+// big commnunity support
+// java is a popular language is it created in 1995 
