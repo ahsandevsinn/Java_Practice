47cebc6b37a7359e3cae7e6038abf200e00191cb
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2024-12-31 14:05:09 -0800

    program 2

--- introduction/task_one.java
+++ introduction/task_one.java
@@ -2,7 +2,14 @@ public class task_one {
 
     // create a class to output good morning 
 
-   static void taskOne(){
-        System.out.println("Good Morning");
+//    static void taskOne(){
+//         System.out.println("Good Morning");
+//     }
+
+    // create a class to output subscribe
+    
+    static void taskOne(){
+        System.out.println("Subscribe");
     }
+
 }
