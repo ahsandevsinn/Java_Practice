78beba1b6d360d4cc04ad02dc36d005fbce70ad2
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2024-12-31 14:11:54 -0800

    program 4

--- introduction/task_one.java
+++ introduction/task_one.java
@@ -1,17 +1,14 @@
+
 public class task_one {
 
     // create a class to output good morning 
-
 //    static void taskOne(){
 //         System.out.println("Good Morning");
 //     }
-
     // create a class to output subscribe
-    
     // static void taskOne(){
     //     System.out.println("Subscribe");
     // }
-
     // right half pyramid
     // static void taskOne(){
     //     System.out.println();
@@ -21,5 +18,13 @@ public class task_one {
     //     System.out.println("* * * *");
     //     System.out.println("* * * * *");
     // }
-
+    //reverse right half pyramid
+    static void taskOne() {
+     System.out.println();
+    System.out.println("* * * * *");
+     System.out.println("* * * *");
+     System.out.println("* * *");
+     System.out.println("* *");
+     System.out.println("*");
+    }
 }
