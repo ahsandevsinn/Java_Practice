5ec47fd30b6f8f2a3cf6997e2406c2e92a48b5aa
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-01-05 03:29:34 -0800

    3 programming challenges

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
+
 public class Main{
  public static void main(String[] args) {
   //  task_one.taskOne();
-  Variables.variablesDataType();
- }
+//   Variables.variablesDataType();
+challengefour.leftHalfPyramid();
+
+}
 }
---
+++ challengefour.java
@@ -0,0 +1,13 @@
+public class challengefour {
+    public static void rightHalfPyramid() {
+        System.out.println("* \n * * \n * * * \n * * * * \n * * * * *");
+    }
+
+    public static void reserveRightHalfPyramid(){
+        System.out.println("* * * * * \n* * * *\n* * *\n* *\n*");
+    }
+
+    public static void leftHalfPyramid(){
+        System.out.println("       *\n      * *\n    * * *\n  * * * *\n* * * * *");
+    }
+}
