9a4f9b29679c4830aafc418eebdb75b714716437
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-01-05 11:06:00 -0800

    input challenge one

--- UserInput.java
+++ UserInput.java
@@ -2,15 +2,23 @@
 import java.util.Scanner;
 
 public class UserInput{
-    public static void userInput(){
-        Scanner input =  new Scanner(System.in);
-        System.out.println("Hi Whats Your Name?");
-       String name = input.nextLine();
-       System.out.println("My Name is" + name);
-       System.out.println("Whats your age ?");
-       int age = input.nextInt();
+    // public static void userInput(){
+    //     Scanner input =  new Scanner(System.in);
+    //     System.out.println("Hi Whats Your Name?");
+    //    String name = input.nextLine();
+    //    System.out.println("My Name is" + name);
+    //    System.out.println("Whats your age ?");
+    //    int age = input.nextInt();
 
-    System.out.println("My age is " + age);
-    System.out.println("My Name is \s" + name  + "\s"+ "My age is\s" + age);
-    }
+    // System.out.println("My age is " + age);
+    // System.out.println("My Name is \s" + name  + "\s"+ "My age is\s" + age);
+    // }
+
+    // create a program to input name of the person and respond with "Welcome Name to KG Coding"
+    // public static void userInput(){
+    //     Scanner input = new Scanner(System.in);
+    //     String name = input.nextLine();
+    //     System.out.println("Welcome\s"+name+"\sto KG Coding");
+
+    // }
 }
