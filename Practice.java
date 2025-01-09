7cfce9722af8c3eeb0932f8fb6c5417526343f20
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-01-08 20:01:33 -0800

    name convertion programs

--- Practice.java
+++ Practice.java
@@ -1,4 +1,3 @@
-import java.util.List;
 
 public class Practice {
     // 🏬 Data Types, Variables & Input (repeat again)
@@ -30,13 +29,10 @@ public class Practice {
 //    System.out.println(e);
 //    System.out.println(f);
 //     }
-   
 // 🖨️ Variables
-
 // Score: 3/5 repeat again
 // What is a variable, and why do we use it in programming?
 //  Ans : Variable is a container to store value
-
 // Write a program to declare, assign, and reassign a variable.
 // 4.5 out of 5
 //  public static void variables(){
@@ -45,30 +41,24 @@ public class Practice {
 //     System.out.println(a);
 //     a = 20;  // (reassign)
 //     System.out.println(a);
-
 //  }
-   
 // Explain the concept of variable scope with an example.
 // 3.5/5
 // int a = 10;
 // public static void scope(){
 //     System.out.println(a);
 // }
-
 // 🚚 Data Types
-
 // Classify the following as integers, floats, strings, or booleans: 42, 3.14, "Hello, World!", False.
 // scores 4/5
 //  42 is an integer
 //  3.14 is an float 
 //  "hello world" is an String
 // false is an boolean
-
 // What is the difference between an integer and a float? Write a program to demonstrate.
 // 3/5
 // integer store wihtout decimal value and integer store only 4 bytes
 // fload store with decimal value and float store only 4 bytes
-
 // Create a list of mixed data types and print each item along with its type.
 // repeat again
 // public static void displayTypes(){
@@ -87,22 +77,30 @@ public class Practice {
 //        System.out.println(e);
 //        System.out.println(f);
 //         }
-
 // ⚠️ Naming Conventions
-// Write a program using camelCase, PascalCase, and snake_case naming conventions for variables.
 // Identify the invalid variable names: 1name, _user, firstName, last name.
 // 2/5
 // lname invalid
 // _user invalid
 //  firstName valid
 // last name invalid
+    // Write a program using camelCase, PascalCase, and snake_case naming conventions for variables.
+    // Identify the invalid variable names: 1name, _user, firstName, last name.
+    // lname invalid
+    // _user valid
+    //  firstName valid
+    // last name invalid
+    // Write a program using camelCase, PascalCase, and snake_case naming conventions for variables.
+    // 3/5
+    // public static void nameConvertion() {
+    //     System.out.println("this is a camelCase function");
+    // }
 
- // Write a program using camelCase, PascalCase, and snake_case naming conventions for variables.
-// Identify the invalid variable names: 1name, _user, firstName, last name.
-// lname invalid
-// _user valid
-//  firstName valid
-// last name invalid
+    // public static void NameConvertion() {
+    //     System.out.println("this is a Pascal Case function");
+    // }
 
-
+    // public static void name_Convertion() {
+    //     System.out.println("this is a snake_Case function");
+    // }
 }
