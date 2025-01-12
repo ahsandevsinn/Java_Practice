ed82cbb2a5498fd5d802a234b0a6d11df3c87107
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-01-12 03:25:11 -0800

    shorthand operator

--- Main.java
+++ Main.java
@@ -1,16 +1,16 @@
 
-public class Main{
- public static void main(String[] args) {
-  //  task_one.taskOne();
-//   Variables.variablesDataType();
-// challengefour.leftHalfPyramid();
-// UserInput.userInput();
-// TypeConvertion.TypeConvert();
-// Practice.displayTypes();
-// Practice.variables();
-// ChallengeSeven.swap();
-// Practice.userInput();
-// ArthimeticOperator.operators();
-OrderOfOperations.orderOperations();
-}
-}
+// public class Main{
+//  public static void main(String[] args) {
+//   //  task_one.taskOne();
+// //   Variables.variablesDataType();
+// // challengefour.leftHalfPyramid();
+// // UserInput.userInput();
+// // TypeConvertion.TypeConvert();
+// // Practice.displayTypes();
+// // Practice.variables();
+// // ChallengeSeven.swap();
+// // Practice.userInput();
+// // ArthimeticOperator.operators();
+// OrderOfOperations.orderOperations();
+// }
+// }
---
+++ ShorthandOperator.java
@@ -0,0 +1,20 @@
+
+public class ShorthandOperator {
+
+    public static void main(String[] args) {
+        int a = 5;
+        a += 1;  // 6
+        System.out.println(a);
+        a -= 1;
+                  // -6
+        System.out.println(a);
+        a *= 1;
+        System.out.println(a);
+        a /= 1;
+
+        System.out.println(a);
+        a %= 1;
+        System.out.println(a);
+
+    }
+}
