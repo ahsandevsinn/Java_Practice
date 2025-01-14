e6096aaaaa605c5f0d8137c1ddfb88bc9229018b
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-01-13 19:24:57 -0800

    Perimter program

--- OperatorsChallenge.java
+++ OperatorsChallenge.java
@@ -13,6 +13,14 @@ public class OperatorsChallenge {
     // float a = 10.0f;
     // float b = 4.0f;
     // System.out.println(a*b);
+
+    // // Create a program to calculate Perimeter of a rectangle Perimeter of rectangle ABCD = A+B+C+D
+    // int a = 10;
+    // int b = 20;
+    // int c = 30;
+    // int d = 40;
+
+    // System.out.println(a+b+c+d);
     
 
 
