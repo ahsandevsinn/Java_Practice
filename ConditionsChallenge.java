7f7d653ba89b13d22777cd537802bccb03ed0a6a
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-01-14 19:59:25 -0800

    greatest of thr three

--- ConditionsChallenge.java
+++ ConditionsChallenge.java
@@ -23,6 +23,24 @@ public class ConditionsChallenge {
         //     System.out.println("Your number is even");
         // } else {
         //     System.out.println("Your number is odd");
+        // }
+        // greatest of the three
+        // Scanner input = new Scanner(System.in);
+        // System.out.println("Please enter num one :");
+        // int num1 = input.nextInt();
+        // System.out.println("Please enter num two :");
+        // int num2 = input.nextInt();
+        // System.out.println("Please enter num three :");
+        // int num3 = input.nextInt();
+
+        // if (num1 >= num2 && num1 >= num3) {
+        //     System.out.println("Num1 is greater than");
+
+        // } else if (num2 >= num3 && num2 >= num1) {
+        //     System.out.println("Num2 is greater than");
+
+        // } else if (num3 >= num1 && num3 >= num2) {
+        //     System.out.println("Num3 is greater than");
 
         // }
     }
