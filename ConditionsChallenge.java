d65cc051e37039e4c502b4fc90c7767a3ffaa5e3
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-01-15 19:54:35 -0800

    person age checking

--- ConditionsChallenge.java
+++ ConditionsChallenge.java
@@ -32,50 +32,54 @@ public class ConditionsChallenge {
         // int num2 = input.nextInt();
         // System.out.println("Please enter num three :");
         // int num3 = input.nextInt();
-
         // if (num1 >= num2 && num1 >= num3) {
         //     System.out.println("Num1 is greater than");
-
         // } else if (num2 >= num3 && num2 >= num1) {
         //     System.out.println("Num2 is greater than");
-
         // } else if (num3 >= num1 && num3 >= num2) {
         //     System.out.println("Num3 is greater than");
-
         // }
-
         // check leap year
         // Scanner input = new Scanner(System.in);
         // int num = input.nextInt();
-
         // if (num % 400 == 0 || (num % 4 == 0 && num % 100 != 0)) {
         //     System.out.println("Your year is leap year");
         // }else {
         //     System.out.println("Your year is not a leap year");
-
         // }
-
         // check student grade
+        // Scanner input = new Scanner(System.in);
+        // System.out.println("Please enter percentage : ");
+        // float percentage = input.nextFloat();
+        // if (percentage >= 90) {
+        //     System.out.println("You have got A Grade");
+        // } else if (percentage >= 75){
+        //     System.out.println("You have got B Grade");
+        // }else if (percentage >= 50) {
+        //     System.out.println("You have got C Grade");
+        // }else if (percentage >= 30) {
+        //     System.out.println("You have got D Grade");
+        // }else {
+        //     System.out.println("You have got F Grade Failed");
+        // }
+        // check age 
         Scanner input = new Scanner(System.in);
-        System.out.println("Please enter percentage : ");
-        float percentage = input.nextFloat();
-        if (percentage >= 90) {
-            System.out.println("You have got A Grade");
-        } else if (percentage >= 75){
-            System.out.println("You have got B Grade");
+        System.out.println("Please enter age : ");
+        int age = input.nextInt();
 
-        }else if (percentage >= 50) {
-            System.out.println("You have got C Grade");
+        if (age >= 60) {
+        System.out.println("You are senior ");
             
-        }else if (percentage >= 30) {
-            System.out.println("You have got D Grade");
+        } else if (age > 20){
+        System.out.println("You are adult");
 
-            
+        }else if (age >= 13){
+        System.out.println("You are Teen");
+
         }else {
-            System.out.println("You have got F Grade Failed");
+        System.out.println("You are child");
 
         }
-
 
     }
 }
