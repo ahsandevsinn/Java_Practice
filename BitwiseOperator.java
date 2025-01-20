88ea3b7e74edbb23ada8bcfce47a6b19dc50de0d
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-01-19 20:03:36 -0800

    bitwise NOT of two numbers

--- BitwiseOperator.java
+++ BitwiseOperator.java
@@ -29,6 +29,15 @@ public class BitwiseOperator {
     //     int result = first ^ second ;
     // System.out.println(result);
 
+    // / create a program that shows bitwise NOT of two numbers 
+    //     Scanner input = new Scanner(System.in);
+    //         System.out.println("Please enter first number :");
+    //        int first = input.nextInt();
+    //        System.out.println("Please enter second number :");
+    //        int second = input.nextInt();
+    //     int result = ~first;
+    // System.out.println(result);
+
 
         
     }
