c683b6afa578bf9401fc9704e70af07e2134461c
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-01-19 19:55:12 -0800

    bitwise OR of two numbers

--- BitwiseOperator.java
+++ BitwiseOperator.java
@@ -3,13 +3,24 @@ import java.util.Scanner;
 public class BitwiseOperator {
     public static void main(String[] args) {
         // create a program that shows bitwise AND of two numbers 
-        Scanner input = new Scanner(System.in);
-        System.out.println("Please enter first number :");
-       int first = input.nextInt();
-       System.out.println("Please enter second number :");
-       int second = input.nextInt();
-    int result = first & second ;
-    System.out.println(result);
+        // Scanner input = new Scanner(System.in);
+    //     System.out.println("Please enter first number :");
+    //    int first = input.nextInt();
+    //    System.out.println("Please enter second number :");
+    //    int second = input.nextInt();
+    // int result = first & second ;
+    // System.out.println(result);
+
+        // create a program that shows bitwise OR of two numbers 
+    //     Scanner input = new Scanner(System.in);
+    //         System.out.println("Please enter first number :");
+    //        int first = input.nextInt();
+    //        System.out.println("Please enter second number :");
+    //        int second = input.nextInt();
+    //     int result = first | second ;
+    // System.out.println(result);
+
+
 
         
     }
