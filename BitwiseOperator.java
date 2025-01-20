5c512ab0d80f5e0581c30a8091704beb3b50c10a
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-01-19 20:28:25 -0800

    check even odd number using bitwise operator

--- BitwiseOperator.java
+++ BitwiseOperator.java
@@ -1,61 +1,68 @@
+
 import java.util.Scanner;
 
 public class BitwiseOperator {
+
     public static void main(String[] args) {
         // create a program that shows bitwise AND of two numbers 
         // Scanner input = new Scanner(System.in);
-    //     System.out.println("Please enter first number :");
-    //    int first = input.nextInt();
-    //    System.out.println("Please enter second number :");
-    //    int second = input.nextInt();
-    // int result = first & second ;
-    // System.out.println(result);
+        //     System.out.println("Please enter first number :");
+        //    int first = input.nextInt();
+        //    System.out.println("Please enter second number :");
+        //    int second = input.nextInt();
+        // int result = first & second ;
+        // System.out.println(result);
 
         // create a program that shows bitwise OR of two numbers 
-    //     Scanner input = new Scanner(System.in);
-    //         System.out.println("Please enter first number :");
-    //        int first = input.nextInt();
-    //        System.out.println("Please enter second number :");
-    //        int second = input.nextInt();
-    //     int result = first | second ;
-    // System.out.println(result);
-
-      // create a program that shows bitwise XOR of two numbers 
-    //     Scanner input = new Scanner(System.in);
-    //         System.out.println("Please enter first number :");
-    //        int first = input.nextInt();
-    //        System.out.println("Please enter second number :");
-    //        int second = input.nextInt();
-    //     int result = first ^ second ;
-    // System.out.println(result);
-
-    // / create a program that shows bitwise NOT of two numbers 
-    //     Scanner input = new Scanner(System.in);
-    //         System.out.println("Please enter first number :");
-    //        int first = input.nextInt();
-    //        System.out.println("Please enter second number :");
-    //        int second = input.nextInt();
-    //     int result = ~first;
-    // System.out.println(result);
-
-
-    // / create a program that shows bitwise left shift of  number 
-    //     Scanner input = new Scanner(System.in);
-    //         System.out.println("Please enter first number :");
-    //        int first = input.nextInt();
-           
-    //     int result = first << 2;
-    // System.out.println(result);
-
-    // / create a program that shows bitwise right shift of  number 
-
+        //     Scanner input = new Scanner(System.in);
+        //         System.out.println("Please enter first number :");
+        //        int first = input.nextInt();
+        //        System.out.println("Please enter second number :");
+        //        int second = input.nextInt();
+        //     int result = first | second ;
+        // System.out.println(result);
+        // create a program that shows bitwise XOR of two numbers 
+        //     Scanner input = new Scanner(System.in);
+        //         System.out.println("Please enter first number :");
+        //        int first = input.nextInt();
+        //        System.out.println("Please enter second number :");
+        //        int second = input.nextInt();
+        //     int result = first ^ second ;
+        // System.out.println(result);
+        // / create a program that shows bitwise NOT of two numbers 
+        //     Scanner input = new Scanner(System.in);
+        //         System.out.println("Please enter first number :");
+        //        int first = input.nextInt();
+        //        System.out.println("Please enter second number :");
+        //        int second = input.nextInt();
+        //     int result = ~first;
+        // System.out.println(result);
+        // / create a program that shows bitwise left shift of  number 
+        //     Scanner input = new Scanner(System.in);
+        //         System.out.println("Please enter first number :");
+        //        int first = input.nextInt();
+        //     int result = first << 2;
+        // System.out.println(result);
+        // / create a program that shows bitwise right shift of  number 
 //     Scanner input = new Scanner(System.in);
 //     System.out.println("Please enter first number :");
 //    int first = input.nextInt();
-   
 // int result = first >> 1;
 // System.out.println(result);
+        // / create a program thats check even odd number using bitwise operator
+//         Scanner input = new Scanner(System.in);
+//         System.out.println("Please enter first number :");
+//         int first = input.nextInt();
 
-        
+// // int result = first & 1 == 0;
+//         if ((first & 1) == 0) {
+
+//             System.out.println("Your number is even");
+
+//         } else {
+//             System.out.println("Your number is odd");
+
+//         }
+
     }
 }
