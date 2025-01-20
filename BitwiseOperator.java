b8aaa9e700809df3dd89d98922e4c6dbd22fd15b
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-01-19 19:46:36 -0800

    first program

---
+++ BitwiseOperator.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class BitwiseOperator {
+    public static void main(String[] args) {
+        // create a program that shows bitwise AND of two numbers 
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter first number :");
+       int first = input.nextInt();
+       System.out.println("Please enter second number :");
+       int second = input.nextInt();
+    int result = first & second ;
+    System.out.println(result);
+
+        
+    }
+}
