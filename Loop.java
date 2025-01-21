913eed08c8e0af6e5067fb1d90e1542d887fcd38
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-01-20 19:32:27 -0800

    count while loop

--- Loop.java
+++ Loop.java
@@ -1,10 +1,17 @@
 public class Loop {
     public static void main(String[] args) {
-        int i = 0;
+        // int i = 0;
 
-        while (i <= 10) {
-            System.out.println(i);
-            i = i + 1;
-        }
+        // while (i <= 10) {
+        //     System.out.println(i);
+        //     i = i + 1;
+        // }
+
+        // int count = 500;
+
+        // while (count >= 200) {
+        //     System.out.println(count);
+        //     count = count - 1;
+        // }
     }
 }
