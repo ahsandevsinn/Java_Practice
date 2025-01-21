0901029386b352c5cbf765e48d31bb0c568508bd
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-01-20 20:25:44 -0800

    function

---
+++ Function.java
@@ -0,0 +1,13 @@
+// public class Function {
+//  public static void main(String[] args) {
+//     System.out.println("In main method");
+
+//      greeting();
+//     System.out.println("method finsh");
+
+//  }   
+
+//  public static void greeting(){
+//     System.out.println("Good Morning");
+//  }
+// }
--- Loop.java
+++ Loop.java
@@ -1,31 +1,31 @@
 
-import java.util.Scanner;
+// import java.util.Scanner;
 
-public class Loop {
+// public class Loop {
 
-    public static void main(String[] args) {
-        // int i = 0;
+//     public static void main(String[] args) {
+//         // int i = 0;
 
-        // while (i <= 10) {
-        //     System.out.println(i);
-        //     i = i + 1;
-        // }
-        // int count = 500;
-        // while (count >= 200) {
-        //     System.out.println(count);
-        //     count = count - 1;
-        // }
-        Scanner input = new Scanner(System.in);
+//         // while (i <= 10) {
+//         //     System.out.println(i);
+//         //     i = i + 1;
+//         // }
+//         // int count = 500;
+//         // while (count >= 200) {
+//         //     System.out.println(count);
+//         //     count = count - 1;
+//         // }
+//         // Scanner input = new Scanner(System.in);
 
-        int i = 0;
+//         // int i = 0;
 
-        while (i < 5) {
+//         // while (i < 5) {
 
-            int num = input.nextInt();
-            System.out.println(num);
-            num++;
+//         //     int num = input.nextInt();
+//         //     System.out.println(num);
+//         //     num++;
 
-        }
+//         // }
 
-    }
-}
+//     }
+// }
