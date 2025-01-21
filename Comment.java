462c7010585139ac76cdf469895693f697abbb49
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-01-20 18:49:53 -0800

    multiline or document

--- Comment.java
+++ Comment.java
@@ -2,5 +2,13 @@ public class  Comment {
     public static void main (String args []){
         // THis is a single line comment
     //  System.out.println("This is a single line comment");
+
+   /*
+    * This 
+    is 
+    a multiline comment
+    */
+
+    /**this is a document comment */
     }
 }
