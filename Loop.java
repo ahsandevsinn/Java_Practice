fbd7b59dfd4a5f242a25ae575062eff4661936d8
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-01-20 20:22:19 -0800

    user input loop

--- Loop.java
+++ Loop.java
@@ -1,4 +1,8 @@
+
+import java.util.Scanner;
+
 public class Loop {
+
     public static void main(String[] args) {
         // int i = 0;
 
@@ -6,12 +10,22 @@ public class Loop {
         //     System.out.println(i);
         //     i = i + 1;
         // }
-
         // int count = 500;
-
         // while (count >= 200) {
         //     System.out.println(count);
         //     count = count - 1;
         // }
+        Scanner input = new Scanner(System.in);
+
+        int i = 0;
+
+        while (i < 5) {
+
+            int num = input.nextInt();
+            System.out.println(num);
+            num++;
+
+        }
+
     }
 }
