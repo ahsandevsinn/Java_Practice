3c28af272676588fdd9325316cb89510ca5b5ab9
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-01-21 19:14:19 -0800

    rightPatternLoop

--- Function.java
+++ Function.java
@@ -1,43 +1,58 @@
-public class Function {
- public static void main(String[] args) {
-    System.out.println("In main method");
-    //  rightPattern();
-    //  leftPattern();
-    //  reverseRightHalf();
-    //  greeting();
-    System.out.println("method finsh");
 
- }   
+public class Function {
+
+    public static void main(String[] args) {
+        System.out.println("In main method");
+        //  rightPattern();
+        //  leftPattern();
+        //  reverseRightHalf();
+        //  greeting();
+        rightPatternLoop();
+        System.out.println("method finsh");
+
+    }
 
 //  public static void greeting(){
 //     System.out.println("Good Morning");
 //  }
-
-public static void rightPattern(){
-    System.out.println();
+    public static void rightPattern() {
+        System.out.println();
         System.out.println("*");
         System.out.println("* *");
         System.out.println("* * *");
         System.out.println("* * * *");
         System.out.println("* * * * *");
-}
+    }
 
-public static void leftPattern(){
-    System.out.println();
-    System.out.println("        *");
-    System.out.println("      * *");
-    System.out.println("    * * *");
-    System.out.println("  * * * *");
-    System.out.println("* * * * *");
-}
+    public static void leftPattern() {
+        System.out.println();
+        System.out.println("        *");
+        System.out.println("      * *");
+        System.out.println("    * * *");
+        System.out.println("  * * * *");
+        System.out.println("* * * * *");
+    }
 
+    public static void reverseRightHalf() {
+        System.out.println();
+        System.out.println("* * * * *");
+        System.out.println("* * * *");
+        System.out.println("* * *");
+        System.out.println("* *");
+        System.out.println("*");
+    }
 
-public static void reverseRightHalf(){
-    System.out.println();
-    System.out.println("* * * * *");
-     System.out.println("* * * *");
-     System.out.println("* * *");
-     System.out.println("* *");
-     System.out.println("*");
-}
+    public static void rightPatternLoop() {
+        int rows = 0;
+        while (rows < 5) {
+            System.out.print("*");
+            int i = 0;
+            while (i < rows) {
+                System.out.print(" *");
+                i++;
+            }
+            System.out.println();
+            rows++;
+        }
+    }
 }
