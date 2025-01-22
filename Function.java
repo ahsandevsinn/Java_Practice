597f168fbdd292a9d48152ab4506b36d0b626f57
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-01-21 18:47:13 -0800

    patterns with method

--- Function.java
+++ Function.java
@@ -1,13 +1,43 @@
-// public class Function {
-//  public static void main(String[] args) {
-//     System.out.println("In main method");
+public class Function {
+ public static void main(String[] args) {
+    System.out.println("In main method");
+    //  rightPattern();
+    //  leftPattern();
+    //  reverseRightHalf();
+    //  greeting();
+    System.out.println("method finsh");
 
-//      greeting();
-//     System.out.println("method finsh");
-
-//  }   
+ }   
 
 //  public static void greeting(){
 //     System.out.println("Good Morning");
 //  }
-// }
+
+public static void rightPattern(){
+    System.out.println();
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");
+        System.out.println("* * * * *");
+}
+
+public static void leftPattern(){
+    System.out.println();
+    System.out.println("        *");
+    System.out.println("      * *");
+    System.out.println("    * * *");
+    System.out.println("  * * * *");
+    System.out.println("* * * * *");
+}
+
+
+public static void reverseRightHalf(){
+    System.out.println();
+    System.out.println("* * * * *");
+     System.out.println("* * * *");
+     System.out.println("* * *");
+     System.out.println("* *");
+     System.out.println("*");
+}
+}
