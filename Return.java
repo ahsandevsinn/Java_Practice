58d34179c84ee772fdd2dc7d0adb84d9620d9734
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-01-21 19:55:58 -0800

    sum of two numbers

--- Return.java
+++ Return.java
@@ -3,23 +3,25 @@ import java.util.Scanner;
 
 public class Return {
     public static void main(String[] args) {
-        int first = firstNumber();
-        int second = secondNumber();
-
-        int finalNumber = first + second;
-        System.out.println("Your final number is\s" + finalNumber);
+      int num = sum(10, 9);
+        System.out.println("Your final number is\s" + num);
     }
 
-    public static int firstNumber (){
-        Scanner input = new Scanner(System.in);
-        int firstNumber = input.nextInt();
-        return firstNumber;
-    }
+    // public static int firstNumber (){
+    //     Scanner input = new Scanner(System.in);
+    //     int firstNumber = input.nextInt();
+    //     return firstNumber;
+    // }
 
 
-    public static int secondNumber (){
-        Scanner input = new Scanner(System.in);
-        int secondNumber = input.nextInt();
-        return secondNumber;
+    // public static int secondNumber (){
+    //     Scanner input = new Scanner(System.in);
+    //     int secondNumber = input.nextInt();
+    //     return secondNumber;
+    // }
+
+    public static int sum(int num1 , int num2){
+        return num1+num2;
+
     }
 }
