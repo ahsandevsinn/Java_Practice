729705fdabdb0ce0399e2e779f6a5be54df2f3e4
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-01-26 11:58:08 -0800

    factorial

--- Challenge28.java
+++ Challenge28.java
@@ -1,20 +1,52 @@
- import java.util.Scanner;
 
- class Challenge28 {
-  
+import java.util.Scanner;
+
+class Challenge28 {
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int num = input.nextInt();
-        numbersTable(num);
+        // int sum = sumOfOddNumber(num);
+        // System.out.println("Your sum is =" + sum);
+        long fact = factorial(num);
+        System.out.println(fact);
+// 
+
     }
 
-    public static void numbersTable(int num){
-        int i = 1;
-        while (i <= 10) {
-            System.out.println(num + " X " + i +" = " + (num*i));
+    // public static void numbersTable(int num) {
+    //     int i = 1;
+    //     while (i <= 10) {
+    //         System.out.println(num + " X " + i + " = " + (num * i));
+    //         i++;
+    //     }
+    // }
+    // public static int sumOfOddNumber(int num) {
+    //     int sum = 0;
+    //     int i = 1;
+
+    //     while (i <= num) {
+    //         sum += i;
+    //     i += 2;
+
+    //     }
+    //     return sum;
+        
+
+
+    // }
+
+    public static long factorial(int  num){
+         if (num < 2) {
+            return 1;
+         }
+         long fact = 1;
+         int i = 2;
+         while (i < num) {
+            fact *= i;
             i++;
-            
-        }
+         }
+         return fact;
     }
-    
+
 }
