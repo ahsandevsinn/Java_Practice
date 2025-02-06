ecacc833a83701a5ad70d6e1ada849a0fe7ca75a
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-02-06 13:21:13 -0800

    array using loop

--- Array.java
+++ Array.java
@@ -1,8 +1,13 @@
 public class Array {
 
     public static void main(String[] args) {
-        int[] Arry = new int[1];
-        Arry[0] = 2090;
-        System.out.println(Arry[0]);
+        // int[] Arry = new int[1];
+        // Arry[0] = 2090;
+        int[] num = {1,2,4,5,6};
+        int index = 0;
+        while (index < num.length) {
+            System.out.println(num[index]);
+            index++;
+        }
     }
 }
