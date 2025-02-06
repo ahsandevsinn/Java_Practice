e0be6bc38172af89d3c62b4cf0ae3eff7581454f
Ahsan Khan <121575348+ahsandevsinn@users.noreply.github.com>
2025-02-06 13:14:25 -0800

    array first practice

--- Array.java
+++ Array.java
@@ -1,10 +1,8 @@
 public class Array {
 
     public static void main(String[] args) {
-        // int[] number = new  int[1];
-        // number[0] = 20;
-        // number[1] = 20;
-        int[] myArr = {10,20};
-        System.out.println(myArr[1]);
+        int[] Arry = new int[1];
+        Arry[0] = 2090;
+        System.out.println(Arry[0]);
     }
 }
